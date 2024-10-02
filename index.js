0295278a4252a481b93ad3ee036cccf9b5a6b666
IrineCKimeto <irenekimetoc@gmail.com>
2024-10-02 10:25:57 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// code your solution here
+function superbowlWin(record) {
+    const winningRecord = record.find(obj => obj.result === "W");
+    return winningRecord ? winningRecord.year : undefined;
+  }
+  
